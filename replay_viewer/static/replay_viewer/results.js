faa0a76bc1cd7695137ab55ed916c010d7c53554
Chester Yap <chesterdyap@gmail.com>
2020-02-23 19:34:51 -0600

    Fix sortable UI

---
+++ replay_viewer/static/replay_viewer/results.js
@@ -0,0 +1,32 @@
+$( () => { addSorting() })
+
+function addSorting() {
+  let s = new Sortable($('tbody')[0], {
+    animation: 150
+  });
+
+  $('tr').each((i, el) => {
+    new Sortable(el, {
+        draggable: '.pokemon',
+        animation: 150
+    });
+  });
+
+  $('.remove-button').click( (e, f) => {
+    $(e.target.parentNode.parentNode).remove();
+  });
+
+  // Serialization
+  $('#export').click(() => {
+    // replay.url, pokemon
+    let urls = $('tr a').map((a, b) => b.href);
+    let children = $('tr').map((a, b) => b.children)
+    let res = urls.map((i, url) => {
+      return {
+        'url': url,
+        'team': Array.from(children[i]).slice(2).map((a, b) => $(a).data('pokemon'))
+      };
+    });
+    $('#serialization').html(JSON.stringify([...res]));
+  });
+}
