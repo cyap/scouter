2de0683a6883853064583fc743457990a23540b9
Chester Yap <chesterdyap@gmail.com>
2020-02-24 11:17:32 -0600

    Front-end QOL

--- replay_viewer/static/replay_viewer/results.js
+++ replay_viewer/static/replay_viewer/results.js
@@ -1,6 +1,7 @@
 $( () => {
     addSorting();
     addExport();
+    addExpand();
 });
 
 function addSorting() {
@@ -30,9 +31,47 @@ function addExport() {
     let res = urls.map((i, url) => {
       return {
         'url': url,
-        'team': Array.from(children[i]).slice(2).map((a, b) => $(a).data('pokemon'))
+        'team': Array.from(children[i]).slice(4).map((a, b) => $(a).data('pokemon'))
       };
     });
     $('#serialization').html(JSON.stringify([...res]));
   });
+}
+
+function addExpand() {
+  $('.expand-button').click((e, f) => {
+    if (!$(e.target).hasClass('expanded')) {
+      expandRow($(e.target));
+    }
+    else {
+      compressRow($(e.target));
+    }
+  });
+
+  $('#expand-all').click((e, f) => {
+    let transform = $(e.target).hasClass('expanded') ? compressRow : expandRow;
+    let text = $(e.target).hasClass('expanded') ? 'Expand All' : 'Compress All';
+    $('.expand-button').each((i, button) => { transform($(button)); });
+    $(e.target).text(text);
+    $(e.target).toggleClass('expanded');
+  });
+}
+
+function expandRow(button) {
+  if ($(button).hasClass('expanded')) return;
+  let pokemon_cells = button.parent().parent().find('.pokemon');
+  pokemon_cells.map((i, cell) => {
+    let exp = $(cell).data('export');
+    $(cell).append(`<div class='expand'>${exp}</div>`); // Change to list
+  });
+  button.text('Compress');
+  button.toggleClass('expanded');
+}
+
+function compressRow(button) {
+  if (!$(button).hasClass('expanded')) return;
+  let pokemon_cells = button.parent().parent().find('.pokemon');
+  pokemon_cells.find('.expand').remove();
+  button.text('Expand');
+  button.toggleClass('expanded');
 }
